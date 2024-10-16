579055f67a04deef7ebb923589b04bf9810fae82
rizwan-bitlogix <rizwan.ahmad@ebitlogix.com>
2024-10-17 03:48:55 +0500

    resolved bugs

--- server/get_data.py
+++ server/get_data.py
@@ -707,18 +707,16 @@ def get_insights(date_filter=None):
         df, [
             col for col in competition_columns if 'Video' in col], '', date_filter)
     video_self = calculate_relevant_insights(
-        df, [col for col in list(
-            label_mapping.keys()) if 'Video' in col], '', date_filter)
+        df, [col for col in tv_azteca_columns if 'Video' in col], '', date_filter)
     note_other = calculate_competition_insights(
         df, [
             col for col in competition_columns if 'Note' in col], '', date_filter)
     note_self = calculate_relevant_insights(
-        df, [col for col in list(
-            label_mapping.keys()) if 'Note' in col], '', date_filter)
+        df, [col for col in tv_azteca_columns if 'Note' in col], '', date_filter)
     total_self = calculate_relevant_insights(
-        df, list(label_mapping.keys()), '', date_filter)
+        df, tv_azteca_columns, '', date_filter)
     total_competition = calculate_competition_insights(df, [
-        col for col in competition_columns if col not in list(label_mapping.keys())], '', date_filter)
+        col for col in competition_columns ], '', date_filter)
     return {
         "videos": {
             "self": video_self,
