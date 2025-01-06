d6b798fcb8dd49dac2134ff093abfd6f3e310e5c
Wasiq Bin Zahid <wasiqibnzahid@gmail.com>
2025-01-06 07:18:50 +0500

    A

--- server/models.py
+++ server/models.py
@@ -206,7 +206,7 @@ class WebsiteCheck(models.Model):
     metrics = models.JSONField(default=dict)
 
     def __str__(self):
-        return f"{self.url} - {self.status} - {self.metrics}"
+        return f"{self.url} - {self.status}"
 
 
 class LastJobRun(models.Model):
