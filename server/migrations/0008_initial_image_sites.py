233759ba86b6888e1867792383227c39ad741813
Wasiq Bin Zahid <wasiqibnzahid@gmail.com>
2025-01-23 03:58:32 +0500

    A

--- server/migrations/0008_initial_image_sites.py
+++ server/migrations/0008_initial_image_sites.py
@@ -5,10 +5,7 @@ def create_initial_sites(apps, schema_editor):
     sites_data = [
         ('Azteca Uno', 'https://www.tvazteca.com/aztecauno/sitemap-image.xml'),
         ('Azteca 7', 'https://www.tvazteca.com/azteca7/sitemap-image.xml'),
-        ('Azteca Deportes', 'https://www.tvazteca.com/aztecadeportes/sitemap-image.xml'),
-        ('ADN 40', 'https://www.adn40.mx/sitemap-image.xml'),
         ('Azteca Noticias', 'https://www.tvazteca.com/aztecanoticias/sitemap-image.xml'),
-        ('Amas TV', 'https://www.tvazteca.com/amastv/sitemap-image.xml'),
     ]
     
     for name, url in sites_data:
