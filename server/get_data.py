eeddd3202eb7bb29c499b0b1e97718c9c737dd21
Wasiq Bin Zahid <wasiqibnzahid@gmail.com>
2025-03-01 06:45:08 +0500

    A

--- server/get_data.py
+++ server/get_data.py
@@ -298,7 +298,7 @@ def calculate_weekly_averages(df):
                 col for col in tv_azteca_columns if 'Video' in col and company in col]] != 0].mean(
                 axis=1).mean()
             if(company == "Azteca UNO"):
-                print(f"ASDASD {za}")
+                print(f"ASDASD {za} {pd.isna(za)}")
             company_avg_video = round(month_df[[
                 col for col in tv_azteca_columns if 'Video' in col and company in col]][month_df[[
                 col for col in tv_azteca_columns if 'Video' in col and company in col]] != 0].mean(
