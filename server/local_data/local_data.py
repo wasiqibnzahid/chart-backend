5ee76ec2e7f2d95e798456301232e9c9b38d9680
Wasiq Bin Zahid <wasiqibnzahid@gmail.com>
2025-03-01 07:24:08 +0500

    A

--- server/local_data/local_data.py
+++ server/local_data/local_data.py
@@ -148,18 +148,18 @@ def init(inner_data=None):
     df['Competition Avg'] = df[competition_columns].mean(axis=1).round(1)
 
     df['TV Azteca Note Avg'] = df[[
-        col for col in azteca_columns if 'Note' in col]][df[[
-        col for col in azteca_columns if 'Note' in col]] != 0].mean(axis=1).round(1)
+        col for col in azteca_columns if True]][df[[
+        col for col in azteca_columns if True]] != 0].mean(axis=1).round(1)
     df['Competition Note Avg'] = df[[
-        col for col in competition_columns if 'Note' in col]][df[[
-        col for col in competition_columns if 'Note' in col]] != 0].mean(axis=1).round(1)
+        col for col in competition_columns if True]][df[[
+        col for col in competition_columns if True]] != 0].mean(axis=1).round(1)
 
     df['TV Azteca Video Avg'] = df[[
-        col for col in azteca_columns if 'Video' in col]][df[[
-        col for col in azteca_columns if 'Video' in col]] != 0].mean(axis=1).round(1)
+        col for col in azteca_columns if True]][df[[
+        col for col in azteca_columns if True]] != 0].mean(axis=1).round(1)
     df['Competition Video Avg'] = df[[
-        col for col in competition_columns if 'Video' in col]][df[[
-        col for col in competition_columns if 'Video' in col]] != 0].mean(axis=1).round(1)
+        col for col in competition_columns if True]][df[[
+        col for col in competition_columns if True]] != 0].mean(axis=1).round(1)
 
     def pct_change(series):
         return series.pct_change().apply(lambda x: x)
