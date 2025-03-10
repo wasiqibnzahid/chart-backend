d4c7a4b2a7e7e7661a9014f34902d337d78452bb
Wasiq Bin Zahid <wasiqibnzahid@gmail.com>
2025-03-11 00:50:57 +0500

    A

--- server/management/commands/run_app.py
+++ server/management/commands/run_app.py
@@ -54,9 +54,9 @@ class Command(BaseCommand):
                 last_job.save()
                 import os
                 os.system('sudo reboot')
-                while True:
-                    print("Rebooting...")
-                    time.sleep(1)
+                # while True:
+                #     print("Rebooting...")
+                #     time.sleep(1)
 
         # Process pending website checks
         while True:
